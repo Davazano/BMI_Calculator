0ddcf576c3780671361f9678247c722fb74933c5
davideoguche <davideoguche@gmail.com>
2024-02-01 18:27:31 +0100

    Created bmi calculator

---
+++ lib/pages/bmi_calculator.dart
@@ -0,0 +1,17 @@
+import "package:flutter/material.dart";
+
+class BmiCalculator extends StatefulWidget {
+  const BmiCalculator({super.key});
+
+  @override
+  State<BmiCalculator> createState() => _BmiCalculatorState();
+}
+
+class _BmiCalculatorState extends State<BmiCalculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
